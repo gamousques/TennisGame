ba4aeb0391cc56f332be25b434460ab4bce2925a
GASTON <mousques@hotmail.com>
2014-11-08 15:30:35 -0200

    Contiene el codigo correcto (origianl) y los casos de prueba

--- test/tennisgame/TennisGameTest.java
+++ test/tennisgame/TennisGameTest.java
@@ -40,10 +40,9 @@ public class TennisGameTest {
     public void tearDown() {
     }
 
-  /*  
-    @Test
+ @Test
     public void testLoveAll(){
-        System.out.println("Love all");
+        System.out.println("1. - Love all");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Love all";
         String result = game.getScore();
@@ -53,7 +52,7 @@ public class TennisGameTest {
     
     @Test
     public void testFifteenLove(){
-        System.out.println("Fifteen,Love");
+        System.out.println("2- Pedro - Fifteen,Love");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Fifteen,Love";
         game.playerOneScores();  //15 0
@@ -63,7 +62,7 @@ public class TennisGameTest {
  
      @Test
     public void testFiftennAll(){
-        System.out.println("Fifteen,all");
+        System.out.println("3- Fifteen,all");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Fifteen all";
         game.playerOneScores(); //15 0
@@ -75,7 +74,7 @@ public class TennisGameTest {
  
     @Test
     public void testFortyThirty(){
-        System.out.println("Forty,Thirty");
+        System.out.println("4- Pedro Forty,Thirty");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Forty,Thirty";
         game.playerOneScores(); //15 0
@@ -86,72 +85,10 @@ public class TennisGameTest {
         String result = game.getScore();
         assertEquals(expResult, result);
     }
-
-     @Test
-    public void testThirtyForty(){
-        System.out.println("Thirty, Forty");
-        game = new TennisGame("Pedro", "Juan");
-        String expResult = "Thirty,Forty";
-        game.playerOneScores(); //15 0
-        game.playerOneScores(); //30 0
-        game.playerTwoScores(); //30 15
-        game.playerTwoScores(); //30 30
-        game.playerTwoScores(); //30 40
-        String result = game.getScore();
-        assertEquals(expResult, result);
-    }
-   
     
-     @Test
-    public void testDuece(){
-        System.out.println("Deuce");
-        game = new TennisGame("Pedro", "Juan");
-        String expResult = "Deuce";
-        game.playerOneScores(); //15 0
-        game.playerOneScores(); //30 0
-        game.playerTwoScores(); //30 15
-        game.playerTwoScores(); //30 30
-        game.playerTwoScores(); //30 40
-        game.playerOneScores(); //40 40
-        String result = game.getScore();
-        assertEquals(expResult, result);
-    }
-   
-     @Test
-    public void testAdvantagePedro(){
-        System.out.println("Advantage Pedro");
-        game = new TennisGame("Pedro", "Juan");
-        String expResult = "Advantage Pedro";
-        game.playerOneScores(); //15 0
-        game.playerOneScores(); //30 0
-        game.playerTwoScores(); //30 15
-        game.playerTwoScores(); //30 30
-        game.playerTwoScores(); //30 40
-        game.playerOneScores(); //40 40
-        game.playerOneScores(); //ad 40
-        String result = game.getScore();
-        assertEquals(expResult, result);
-    }
-    
-      @Test
-    public void testAdvantageJuan(){
-        System.out.println("Advantage juan");
-        game = new TennisGame("Pedro", "Juan");
-        String expResult = "Advantage Juan";
-        game.playerOneScores(); //15 0
-        game.playerOneScores(); //30 0
-        game.playerTwoScores(); //30 15
-        game.playerTwoScores(); //30 30
-        game.playerTwoScores(); //30 40
-        game.playerOneScores(); //40 40
-        game.playerTwoScores(); //40 av
-        String result = game.getScore();
-        assertEquals(expResult, result);
-    }
-    
-     @Test
+    @Test
     public void testPedroWins(){
-        System.out.println("Pedro Wins");
+        System.out.println("5- Pedro wins");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Pedro wins";
         game.playerOneScores(); //15 0
@@ -163,24 +100,91 @@ public class TennisGameTest {
         String result = game.getScore();
         assertEquals(expResult, result);
     }
-
-    @Test
+    
+     @Test
+    public void testThirtyForty(){
+        System.out.println("6- Juan Thirty, Forty");
+        game = new TennisGame("Pedro", "Juan");
+        String expResult = "Thirty,Forty";
+        game.playerOneScores(); //15 0
+        game.playerOneScores(); //30 0
+        game.playerTwoScores(); //30 15
+        game.playerTwoScores(); //30 30
+        game.playerTwoScores(); //30 40
+        String result = game.getScore();
+        assertEquals(expResult, result);
+    }
+   
+     @Test
     public void testJuanWins(){
-        System.out.println("Juan wins");
+        System.out.println("7- Juan wins");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Juan wins";
         game.playerOneScores(); //15 0
-        game.playerTwoScores(); //15 15
+        game.playerOneScores(); //30 0
         game.playerTwoScores(); //30 15
-        game.playerTwoScores(); //40 15
-        game.playerTwoScores(); //win 15
+        game.playerTwoScores(); //30 30
+        game.playerTwoScores(); //30 40
+        game.playerTwoScores(); //30 wins
+        String result = game.getScore();
+        assertEquals(expResult, result);
+    }
+   
+    
+     @Test
+    public void testDuece(){
+        System.out.println("8- Deuce");
+        game = new TennisGame("Pedro", "Juan");
+        String expResult = "Deuce";
+        game.playerOneScores(); //15 0
+        game.playerTwoScores(); //15 15
+        game.playerOneScores(); //30 15
+        game.playerTwoScores(); //30 30
+        game.playerOneScores(); //40 30
+        game.playerTwoScores(); //40 40
+     
+        String result = game.getScore();
+        assertEquals(expResult, result);
+    }
+   
+     @Test
+    public void testAdvantagePedro(){
+        System.out.println("9- Advantage Pedro");
+        game = new TennisGame("Pedro", "Juan");
+        String expResult = "Advantage Pedro";
+        game.playerOneScores(); //15 0
+        game.playerTwoScores(); //15 15
+        game.playerOneScores(); //30 15
+        game.playerTwoScores(); //30 30
+        game.playerOneScores(); //40 30
+        game.playerTwoScores(); //40 40
+        game.playerOneScores(); //ad 40
+
         String result = game.getScore();
         assertEquals(expResult, result);
     }
     
+      @Test
+    public void testAdvantageJuan(){
+        System.out.println("10 Advantage juan");
+        game = new TennisGame("Pedro", "Juan");
+        String expResult = "Advantage Juan";
+        game.playerOneScores(); //15 0
+        game.playerTwoScores(); //15 15
+        game.playerOneScores(); //30 15
+        game.playerTwoScores(); //30 30
+        game.playerOneScores(); //40 30
+        game.playerTwoScores(); //40 40
+        game.playerTwoScores(); //40 av
+        String result = game.getScore();
+        assertEquals(expResult, result);
+    }
+    
+     
+    // TEST PARTICION INVALIDA
      @Test
     public void testPuntajeMayorPedroMasde40(){
-        System.out.println("PuntajePedro > 40");
+        System.out.println("I1 - PuntajePedro > 40");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Wrong Score";
         game.playerOneScores(); //15 0
@@ -193,7 +197,7 @@ public class TennisGameTest {
     
    @Test
     public void testPuntajeMayorJuanMasde40(){
-        System.out.println("PuntajeJuan > 40");
+        System.out.println("I2 PuntajeJuan > 40");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Wrong Score";
         game.playerTwoScores(); //15
@@ -206,7 +210,7 @@ public class TennisGameTest {
     
     @Test
     public void testPuntajeMayorEnAdvantageJuan(){
-        System.out.println("PuntajeMayorEnAdvantageJuan");
+        System.out.println("I3 PuntajeMayorEnAdvantageJuan");
         game = new TennisGame("Pedro", "Juan");
         String expResult = "Wrong Score";
         game.playerOneScores(); //15 0
@@ -220,14 +224,5 @@ public class TennisGameTest {
         String result = game.getScore();
         assertEquals(expResult, result);
     }
-*/
-    /**
-     * Test of getScore method, of class TennisGame.
-     */
- 
 
-    /**
-     * Test of playerOneScores method, of class TennisGame.
-     */
- 
 }
